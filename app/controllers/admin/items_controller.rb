fc09258a1abc0186b435edfc17a88ed9ea58b410
siragasnow <siraga613@gmail.com>
2020-09-24 16:43:35 +0900

    [add]admin item index,show,edit page

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,10 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -16,9 +18,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
 	belongs_to :genre
+
+	enum is_sales_active: {
+		販売中: 0,
+		販売停止中: 1,
+	}
 end
