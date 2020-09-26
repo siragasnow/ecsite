ceba26da2f047739946118e95981443c0467963a
siragasnow <siraga613@gmail.com>
2020-09-26 20:34:30 +0900

    [add]admin genre edit page

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,22 +5,26 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-  	@genre = Genre.new(genres_params)
+  	@genre = Genre.new(genre_params)
   	@genre.save
   	redirect_to admin_genres_path
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   def destroy
   end
 
   private
-  def genres_params
+  def genre_params
   	params.require(:genre).permit(:name, :is_active)
   end
 
