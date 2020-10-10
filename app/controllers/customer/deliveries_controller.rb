88bc401992934438128dc166bfafacd6032ddedd
siragasnow <siraga613@gmail.com>
2020-10-10 16:45:47 +0900

    [fix]delivery page

--- app/controllers/customer/deliveries_controller.rb
+++ app/controllers/customer/deliveries_controller.rb
@@ -1,20 +1,38 @@
 class Customer::DeliveriesController < ApplicationController
   def index
-    @delivery = Delivery.new
-    @deliveries = current_customer.deliveries
+    @customer = Customer.find(current_customer.id)
+      @delivery = Delivery.new
+      @deliveries = @customer.deliveries
   end
 
   def create
-    @deliveries = current_customer.deliveries
-    @delivery = Delivery.new
+    @delivery = Delivery.new(delivery_params)
+      @delivery.customer_id = current_customer.id
+      @delivery.save
+      redirect_to customers_deliveries_path(@delivery.id)
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
+      @delivery = Delivery.find(params[:id])
   end
 
   def update
+    @delivery = Delivery.find(params[:id])
+      @delivery.update(delivery_params)
+      redirect_to customers_deliveries_path
   end
 
-  def destory
+  def destroy
+    @delivery = Delivery.find(params[:id])
+      @delivery.destroy
+      redirect_back(fallback_location: root_path)
   end
+
+  private
+    def delivery_params
+      params.require(:delivery).permit(:postal_code, :destination, :name)
+    end
+
+
 end
--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -29,7 +29,7 @@ class Customer::SessionsController < Devise::SessionsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+   def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+   end
 end
