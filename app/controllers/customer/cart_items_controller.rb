c2524f58cebf67be23fcdcb102a0384d30919070
siragasnow <siraga613@gmail.com>
2020-10-31 17:05:07 +0900

    [add]customer/cartItem controller

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,8 +1,21 @@
 class Customer::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items.includes(:item)
   end
 
   def create
+    if @cart_item
+      new_amount = @cart_item.amount + cart_item_params[:amount]
+      @cart_item.update(amount: new_amount)
+      redirect_to cart_items_path
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+      @cart_item.item_id = @item.id
+      if @cart_item.save
+        redirect_to cart_items_path
+      else
+        render 'customers/items/show'
+      end
   end
 
   def update
@@ -12,5 +25,14 @@ class Customer::CartItemsController < ApplicationController
   end
 
   def destroy_all
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount)
+  end
+
+  def set_cart_item
+    @item = Item.find(params[:item_id])
+    @cart_item = current_customer.has_in_cart(@item)
   end
 end
